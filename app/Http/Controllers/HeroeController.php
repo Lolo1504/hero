8dbb40206d7e13ac7c9d07fd1381a63ca06e4270
lolo03 <manulag46@gmail.com>
2024-03-26 13:25:24 +0100

    segundo commit

---
+++ app/Http/Controllers/HeroeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HeroeController extends Controller
+{
+
+    public function index(){
+        return view("admin.heroes.index");
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,17 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\EnemigoController;
+use App\Http\Controllers\HeroeController;
+use App\Http\Controllers\ItemController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route :: get('/home/{name}', function($name)
-{
-    return view ('home',['name'=> $name]);
-    #return "Esto es el home con nombre " . "$name";
-})->where ('name', '[A-Za-z0-9]+');
+Route::get('/admin', [AdminController::class,'index'])-> name("admin");
+Route::get('/admin/heroes', [HeroeController::class,'index']) ->name("admin.heroes");
+Route::get('/admin/enemigos', [EnemigoController::class,'index'])-> name("admin.enemigos");
+Route::get('/admin/items', [ItemController::class,'index'])->name("admin.items");
+
